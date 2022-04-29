bf82af87917e668d438db535dd9170bc62533037
QuentinMlt <quentinmaillotpro@gmail.com>
2022-04-29 10:30:44 +0200

    Correction index.js avec __dirname

--- exo4/src/index.js
+++ exo4/src/index.js
@@ -1,11 +1,11 @@
 var path = require('path');
 var fs = require('fs')
 
-var log = path.join('/home', 'quentin', 'intronode', 'exo4', 'src', 'log.txt');
+var log = path.join(__dirname, 'log.txt');
 console.log("Fichier log.txt :")
 console.log(fs.readFileSync(log, {encoding: "utf-8"}));
 
-var html = path.join('/home', 'quentin', 'intronode', 'exo4', 'assets', 'index.html');
+var html = path.join(__dirname, '../assets/index.html');
 console.log("Fichier index.html :")
 console.log(fs.readFileSync(html, {encoding: "utf-8"}));
 
