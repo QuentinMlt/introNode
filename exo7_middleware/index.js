8b00932bb12fc3128561d8c7a9129f602a6f8dd4
QuentinMlt <quentinmaillotpro@gmail.com>
2022-04-29 16:53:12 +0200

    ajout middleware chronometre

--- exo7_middleware/index.js
+++ exo7_middleware/index.js
@@ -1,25 +1,29 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+// ICI MIDDLEWARE A EXECUTER AVANT DE TRAITER LA REQUETE.
+  let start = performance.now();
   try {
-    // ICI MIDDLEWARE A EXECUTER AVANT DE TRAITER LA REQUETE.
     console.log(req.httpVersion, req.url, req.method);
     // FIN MIDDLEWARE
     if (req.url === "/") {
       res.writeHead(200, {'content-type':'text/html'});
       res.write('<h1>HOMEPAGE</h1>');
-      res.end();
     } else {
       res.writeHead(404, {'content-type':'text/html'});
       res.write('<h1>404 Not Found</h1>');
-      res.end()
     }
   } catch (err) {
     res.writeHead(500, {'content-type':'text/html'});
     res.write('<h1>500 Internal Server Error</h1>');
-    res.end()
   }
-} );
+  // ICI MIDDLEWARE A EXECUTER AVANT D'ENVOYER LA REPONSE
+  //
+  res.end()
+  // ICI MIDDLEWARE A EXECUTER APRES AVOIR ENVOYER LA REPONSE
+  let end = performance.now();
+  console.log("requête a pris", end - start);
+})
 
 server.listen(3000);
 console.log('Server écoute sur le port 3000');
